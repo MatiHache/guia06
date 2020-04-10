97200aa536b18ae42ed1f8748d3d9d3654c2e4e2
Matías Hillar <matiashillar@colegioinmaculada.edu.ar>
2020-04-10 17:49:25 -0300

    resuelvo paso 02

--- src/died/guia06/Alumno.java
+++ src/died/guia06/Alumno.java
@@ -2,14 +2,47 @@ package died.guia06;
 
 import java.util.List;
 
+import org.junit.platform.commons.util.StringUtils;
 
-public class Alumno {
+
+public class Alumno implements Comparator<Alumno>{
 
 	private String nombre;
 	private Integer nroLibreta;
 	private List<Curso> cursando;
 	private List<Curso> aprobados;
+	
+	public Alumno() {}
+	
+	public Alumno(String nombre, Integer nroLibreta) {
+		this.nombre = nombre;
+		this.nroLibreta = nroLibreta;
+	}
 
+	public void setCursando(List<Curso> cursando) {
+		this.cursando = cursando;
+	}
+
+	public void setAprobados(List<Curso> aprobados) {
+		this.aprobados = aprobados;
+	}
+
+	public StringUtilsing getNombre() {
+		return nombre;
+	}
+
+	public Integer getNroLibreta() {
+		return nroLibreta;
+	}
+
+	public List<Curso> getCursando() {
+		return cursando;
+	}
+
+	public List<Curso> getAprobados() {
+		return aprobados;
+	}
+	
 	public int creditosObtenidos() {
 		return 1;
 	}
@@ -22,6 +55,16 @@ public class Alumno {
 		//
 	}
 	
+	@Override
+	public boolean equals(Alumno a) {
+		if(this.nroLibreta == (Alumno) a.nroLibreta) return true;
+		else return false;
+	}
 	
+	@Override
+	public int compare(Alumno a1, Alumno a2) {
+		a1 = this;		
+		return a1.getNombre().compareTo(a2.getNombre());
+	}
 
 }
